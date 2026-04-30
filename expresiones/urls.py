c7866249ba661c1ef6ace68371a28bca4b090c9d
Stevens Cambranes <cambranesstevens@gmail.com>
2026-04-29 19:06:25 -0600

    Modulo principal redirigiendo a expresiones

--- expresiones/urls.py
+++ expresiones/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('expresiones/', views.vista_expresiones, name='ruta_expresiones'),
 ]
--- principal/urls.py
+++ principal/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 # asi se hacen las url
 urlpatterns = [
     path('', include('main.urls')),
+    path('', include('expresiones.urls')),
 ]
