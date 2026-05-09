c65ab608f86a12fcf8b29aa97deaf0d3662c7b13
Stevens Cambranes <cambranesstevens@gmail.com>
2026-05-08 18:30:31 -0600

    subiendo a la web

--- principal/settings.py
+++ principal/settings.py
@@ -15,7 +15,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$x4wrubbo13%wnua7%6_uh+@8y@&e5c#@v4&!ber1&gj#!&6(='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['proyectoia-production-3e34.up.railway.app', 'localhost', '127.0.0.1']
 
